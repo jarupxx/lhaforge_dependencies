684959d674739994260309c62b4d85f196c7e0bc
Claybird <claybird.without.wing@gmail.com>
2021-05-02 23:11:09 +0900

    added mini-test app

---
+++ miniapp/miniapp.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#define LIBARCHIVE_STATIC
+#include <libarchive/archive.h>
+#include <libarchive/archive_entry.h>
+
+int main()
+{
+    printf("Libarchive version details:\n");
+    printf("%s\n", archive_version_details());
+
+    struct archive* arc;
+    arc = archive_write_new();
+    archive_write_close(arc);
+    archive_write_free(arc);
+    return 0;
+}
