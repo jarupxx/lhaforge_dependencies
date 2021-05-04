d98559d2c0e49cc97733719e1bf8a07edb7ddbe7
Claybird <claybird.without.wing@gmail.com>
2021-05-04 21:46:37 +0900

    disabled HAVE_LZMA_STREAM_ENCODER_MT
    xz archive creation fails when this enabled

--- libarchive/config.h
+++ libarchive/config.h
@@ -812,7 +812,7 @@ typedef uint64_t uintmax_t;
 #define HAVE_LZMA_H 1
 
 /* Define to 1 if you have a working `lzma_stream_encoder_mt' function. */
-#define HAVE_LZMA_STREAM_ENCODER_MT 1
+/* #undef HAVE_LZMA_STREAM_ENCODER_MT */
 
 /* Define to 1 if you have the <lzo/lzo1x.h> header file. */
 /* #undef HAVE_LZO_LZO1X_H */
