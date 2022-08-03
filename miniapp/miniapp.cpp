eb8e8b8e5030922730d2fef8f4b72b474584177d
Claybird <claybird.without.wing@gmail.com>
2022-08-03 21:18:21 +0900

    added micro test for minizip-ng

--- miniapp/miniapp.cpp
+++ miniapp/miniapp.cpp
@@ -2,9 +2,11 @@
 #define LIBARCHIVE_STATIC
 #include <libarchive/archive.h>
 #include <libarchive/archive_entry.h>
+#include <zip.h>
 
 int main()
 {
+    //libarchive
     printf("Libarchive version details:\n");
     printf("%s\n", archive_version_details());
 
@@ -12,5 +14,9 @@ int main()
     arc = archive_write_new();
     archive_write_close(arc);
     archive_write_free(arc);
+
+    //minizip
+    auto zf = unzOpen64("some_non_existing_file");
+    if (!zf)printf("minizip returned as expected");
     return 0;
 }
